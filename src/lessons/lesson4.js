2df18f53b5124247bdea5203dffd8c0a5c8dd8ab
Yasser Carreón <yacasia@gmail.com>
2019-10-17 14:56:35 -0500

    [LS4] Create playground

---
+++ src/lessons/lesson4.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+
+import Switch from '../Switch';
+
+const ToggleContext = React.createContext();
+
+function ToggleConsumer(props) {
+  return (
+    <ToggleContext.Consumer>
+      {(context) => {
+        if (!context) {
+          throw new Error('Toggle compound components must be rendered within the Toggle component');
+        }
+
+        return props.children(context);
+      }}
+    </ToggleContext.Consumer>
+  );
+}
+
+class Toggle extends Component {
+  static On = ({ children }) => (
+    <ToggleConsumer>
+      {({ on }) => on ? children : null}
+    </ToggleConsumer>
+  );
+  
+  static Off = ({ children }) => (
+    <ToggleConsumer>
+      {({ on }) => on ? null : children}
+    </ToggleConsumer>
+  );
+  
+  static Button = props => (
+    <ToggleConsumer>
+      {({ on, toggle }) => <Switch checked={on} onChange={toggle} {...props} />}
+    </ToggleConsumer>
+  );
+
+  state = { checked: false };
+
+  onChange = () => this.setState(
+    ({ checked }) => ({ checked: !checked }),
+    () => { this.props.onToggle(this.state.checked); },
+  );
+
+  // Use Context to get access to the props regardless of the element location.
+  render() {
+    return (
+      <ToggleContext.Provider value={{ on: this.state.checked, toggle: this.onChange }}>
+        {this.props.children}
+      </ToggleContext.Provider>
+    )
+  }
+}
+  
+function Usage({
+  onToggle = (...args) => console.log('onToggle', ...args),
+}) {
+  return (
+    <Toggle onToggle={onToggle}>
+      <div>
+        <Toggle.On>Toggle is On</Toggle.On>
+        <Toggle.Off>Toggle is Off</Toggle.Off>
+        <div>
+            <Toggle.Button />
+        </div>
+      </div>
+    </Toggle>
+  );
+}
+
+export default Usage;
